ecf2a1e7faff09191f41a10aeeca1dff08f4c136
Getúlio Souza <36246041+Getuliosouzaa@users.noreply.github.com>
2018-05-08 20:42:08 -0300

    EAD4

--- js/Salario.js
+++ js/Salario.js
@@ -21,19 +21,19 @@ class Salario{
             throw new Error('O salario dever ser maior ou igual a zero!!');
         }
     }
-    
+
 // Caculos INSS
     _calculos() {
         if(this._salarioBruto <= 1693.72){
-            return this._desINSS = this._salarioBruto * 0.08;
+            return this._descontoINSS = this._salarioBruto * 0.08;
         }
         if(this._salarioBruto > 1693.72 && this._salarioBruto <=2822.90){
-            return this._desINSS =  this._salarioBruto * 0.09;
+            return this._descontoINSS =  this._salarioBruto * 0.09;
         }
         if(this._salarioBruto > 2822.90 && this._salarioBruto <= 5645.80){
-            return this._desINSS =  this._salarioBruto * 0.11;
+            return this._descontoINSS =  this._salarioBruto * 0.11;
         }else {
-            return this._desINSS =  621.04;
+            return this._descontoINSS =  621.04;
         }
         
         
@@ -41,30 +41,35 @@ class Salario{
     
 // Calculos IRPF
     _calculosIR(){
-        this._baseIRPF= this._salarioBruto - this._desINSS;
+        this._baseIR = this._salarioBruto - this._descontoINSS;
 
-        if(this._baseIRPF >= 1903.99 && this._baseIRPF <= 2826.65){
-            this._descontoIRPF= this._baseIRPF * 0.075;
+        if (this._baseIR <= 1903.98 ) {
+         return  this._deducaoIRPF = 0;
+        
+        }
+
+        if(this._baseIR >= 1903.99 && this._baseIR <= 2826.65){
+            this._descontoIRPF= this._baseIR * 0.075;
             return this._deducaoIRPF =this._descontoIRPF - 142.80;
         }
-        if(this._baseIRPF > 2826.65 && this._baseIRPF <= 3751.05){
-            this._descontoIRPF= this._baseIRPF * 0.15;
+        if(this._baseIR > 2826.65 && this._baseIR <= 3751.05){
+            this._descontoIRPF= this._baseIR * 0.15;
             return  this._deducaoIRPF = this._descontoIRPF - 354.80;
             
         }
-        if(this._baseIRPF > 3751.05 && this._baseIRPF <= 4664.68){
-            this._descontoIRPF= this._baseIRPF * 0.225;
+        if(this._baseIR > 3751.05 && this._baseIR <= 4664.68){
+            this._descontoIRPF= this._baseIR * 0.225;
             return  this._deducaoIRPF = this._descontoIRPF - 636.13; 
             
         }else{
-            this._descontoIRPF= this._baseIRPF * 0.275;
+            this._descontoIRPF= this._baseIR * 0.275;
             return  this._deducaoIRPF = this._descontoIRPF - 869.36;
             
         }
     }
     
     _retorno(){
-         return this.salLiquido = this._salarioBruto - this._desINSS - this._deducaoIRPF;
+         return this.salLiquido = this._salarioBruto - this._descontoINSS - this._deducaoIRPF;
         
     }
   
@@ -73,13 +78,13 @@ get salarioBruto(){
     return this._salarioBruto;
 }
 get descontoINSS(){
-    return Number (this._desINSS.toFixed(2));   
+    return Number (this._descontoINSS.toFixed(2));   
 }
 get descontoIRPF(){
     return Number (this._deducaoIRPF.toFixed(2));   
 }
 get totalDescontos(){
-    return Number ((this._desINSS + this._deducaoIRPF).toFixed(2));   
+    return Number ((this._descontoINSS + this._deducaoIRPF).toFixed(2));   
 }
 get salarioLiquido(){
     return Number (this.salLiquido.toFixed(2));
